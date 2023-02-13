0683afddf9746824247d1cf53ac74aca59b1241d
IslamHassan <eslamwork2019@gmail.com>
2023-02-13 17:21:17 +0200

    for Loops

--- src/main/kotlin/Ranges.kt
+++ src/main/kotlin/Ranges.kt
@@ -3,4 +3,15 @@ fun main() {
     val halfOpenRange = 1 until 5
     println(closedRange)
     println(halfOpenRange)
+
+
+    //For Loops
+    val count =10
+    var sum =0
+    for (i in 1..count){
+        sum +=i
+        println(sum)
+    }
+
+
 }
